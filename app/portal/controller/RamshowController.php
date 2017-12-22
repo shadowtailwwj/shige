43de0efabfdcbee98adda81b86ff364a3885f3fc
shadowtailwwj <wwj631877232@qq.com>
2017-12-22 16:44:28 +0800

    1644

---
+++ app/portal/controller/RamshowController.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 老猫 <thinkcmf@126.com>
+// +----------------------------------------------------------------------
+namespace app\portal\controller;
+
+use cmf\controller\HomeBaseController;
+use app\portal\model\PortalCategoryModel;
+use app\portal\service\PostService;
+use app\portal\model\PortalPostModel;
+use think\Db;
+
+class RamshowController extends HomeBaseController
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+}
