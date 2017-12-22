41f4047662f2d066e86cc37159e9c56c7d45cb73
shadowtailwwj <wwj631877232@qq.com>
2017-12-22 17:31:19 +0800

    1731

--- app/portal/controller/AlbumController.php
+++ app/portal/controller/AlbumController.php
@@ -21,56 +21,11 @@ class AlbumController extends HomeBaseController
     public function index()
     {
 
-        $portalCategoryModel = new PortalCategoryModel();
-        $postService         = new PostService();
-
-        $articleId  = $this->request->param('id', 0, 'intval');
-        $categoryId = $this->request->param('cid', 0, 'intval');
-        $article    = $postService->publishedArticle($articleId, $categoryId);
-
-        if (empty($articleId)) {
-            abort(404, '文章不存在!');
-        }
-
-
-        $prevArticle = $postService->publishedPrevArticle($articleId, $categoryId);
-        $nextArticle = $postService->publishedNextArticle($articleId, $categoryId);
-
-        $tplName = 'article';
-
-        if (empty($categoryId)) {
-            $categories = $article['categories'];
-
-            if (count($categories) > 0) {
-                $this->assign('category', $categories[0]);
-            } else {
-                abort(404, '文章未指定分类!');
-            }
-
-        } else {
-            $category = $portalCategoryModel->where('id', $categoryId)->where('status', 1)->find();
-
-            if (empty($category)) {
-                abort(404, '文章不存在!');
-            }
-
-            $this->assign('category', $category);
-
-            $tplName = empty($category["one_tpl"]) ? $tplName : $category["one_tpl"];
-        }
-
-        Db::name('portal_post')->where(['id' => $articleId])->setInc('post_hits');
-
-
-        hook('portal_before_assign_article', $article);
-
-        $this->assign('article', $article);
-        $this->assign('prev_article', $prevArticle);
-        $this->assign('next_article', $nextArticle);
-
-        $tplName = empty($article['more']['template']) ? $tplName : $article['more']['template'];
-
-        return $this->fetch("/$tplName");
+        $cate = Db::name('portal_category_album')->select();
+        $album = Db::name('portal_album')->where('flag',1)->select();
+        $this->assign('cate', $cate);
+        $this->assign('album', $album);
+        return $this->fetch();
     }
 
     // 文章点赞
