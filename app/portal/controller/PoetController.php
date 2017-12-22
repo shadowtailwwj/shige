a6e2bdd4ad93a92d0eb961d6fd2bb0d999d8f86e
shadowtailwwj <wwj631877232@qq.com>
2017-12-22 17:18:36 +0800

    1718

--- app/portal/controller/PoetController.php
+++ app/portal/controller/PoetController.php
@@ -20,57 +20,11 @@ class PoetController extends HomeBaseController
 {
     public function index()
     {
-
-        $portalCategoryModel = new PortalCategoryModel();
-        $postService         = new PostService();
-
-        $articleId  = $this->request->param('id', 0, 'intval');
-        $categoryId = $this->request->param('cid', 0, 'intval');
-        $article    = $postService->publishedArticle($articleId, $categoryId);
-
-        if (empty($articleId)) {
-            abort(404, '文章不存在!');
-        }
-
-
-        $prevArticle = $postService->publishedPrevArticle($articleId, $categoryId);
-        $nextArticle = $postService->publishedNextArticle($articleId, $categoryId);
-
-        $tplName = 'article';
-
-        if (empty($categoryId)) {
-            $categories = $article['categories'];
-
-            if (count($categories) > 0) {
-                $this->assign('category', $categories[0]);
-            } else {
-                abort(404, '文章未指定分类!');
-            }
-
-        } else {
-            $category = $portalCategoryModel->where('id', $categoryId)->where('status', 1)->find();
-
-            if (empty($category)) {
-                abort(404, '文章不存在!');
-            }
-
-            $this->assign('category', $category);
-
-            $tplName = empty($category["one_tpl"]) ? $tplName : $category["one_tpl"];
-        }
-
-        Db::name('portal_post')->where(['id' => $articleId])->setInc('post_hits');
-
-
-        hook('portal_before_assign_article', $article);
-
-        $this->assign('article', $article);
-        $this->assign('prev_article', $prevArticle);
-        $this->assign('next_article', $nextArticle);
-
-        $tplName = empty($article['more']['template']) ? $tplName : $article['more']['template'];
-
-        return $this->fetch("/$tplName");
+        $times = Db::name('times')->select();
+        $poet = Db::name('portal_poet')->where('flag',1)->select();
+        $this->assign('times', $times);
+        $this->assign('poet', $poet);
+        return $this->fetch();
     }
 
     // 文章点赞
