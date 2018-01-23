38719c78c72f94857c0ee6c310b9e560e37df0d9
shadowtailwwj <wwj631877232@qq.com>
2018-01-23 17:18:38 +0800

    1718

---
+++ app/portal/validate/AdminAdvertValidate.php
@@ -0,0 +1,28 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 小夏 < 449134904@qq.com>
+// +----------------------------------------------------------------------
+namespace app\portal\validate;
+
+use think\Validate;
+
+class AdminAdvertValidate extends Validate
+{
+    protected $rule = [
+        'img' => 'require',
+    ];
+    protected $message = [
+        'img.require' => '焦点图不能为空！',
+    ];
+
+    protected $scene = [
+//        'add'  => ['user_login,user_pass,user_email'],
+//        'edit' => ['user_login,user_email'],
+    ];
+}
